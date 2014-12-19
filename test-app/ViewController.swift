c7cdbf3ad8e050131a53cca2b37b4cb6985a5ec5
Tsukagoshi <tsukagoshi@qwintet.com>
2014-12-19 10:35:39 +0900

    test

--- test-app/ViewController.swift
+++ test-app/ViewController.swift
@@ -1,18 +1,15 @@
-//
-//  ViewController.swift
-//  test-app
-//
-//  Created by 塚越 大祐 on 2014/11/24.
-//  Copyright (c) 2014年 daisuke. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        height.delegate = self
+        weight.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +17,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet var height: UITextField!
+    
 
+    @IBOutlet var weight: UITextField!
+    
+    @IBOutlet var calcBtn: UIButton!
+
+    @IBOutlet var resultArea: UILabel!
+
+    @IBAction func calcBMI(sender: AnyObject) {
+        var heightString = self.height.text
+        var weightString = self.weight.text
+        
+        if(heightString == "" || weightString == ""){
+            resultArea.text = "値を入力してください"
+            return;
+        }
+
+        
+        var heightValue = (heightString as NSString).floatValue
+        var weightValue = (weightString as NSString).floatValue
+
+        var result = calculation(h: heightValue, w: weightValue)
+        
+        var resultText : String
+        resultText = "あなたの身長:\(heightString)cm  体重:\(weightString)kg\n\n"
+        resultText += "BMIは\(result.calcBMI()) です　\n\n"
+        resultText += "あなたの適正体重は\(result.calcStdWight())kg です　\n\n"
+        resultText += "判定：あなたは「\(result.judge())」 です"
+        
+        self.resultArea.numberOfLines = 8
+        self.resultArea.text = resultText
+        
+        return;
+        
+    }
 }
 
--- test-app/calculation.swift
+++ test-app/calculation.swift
@@ -1,9 +1,41 @@
-//
-//  calculation.swift
-//  test-app
-//
-//  Created by 塚越 大祐 on 2014/11/24.
-//  Copyright (c) 2014年 daisuke. All rights reserved.
-//
 
 import Foundation
+
+class calculation {
+    var height : Float?
+    var weight : Float?
+    var BMI : Float?
+    
+    init(h : Float, w: Float){
+        self.height = h/100;
+        self.weight = w;
+    }
+    
+    func calcBMI() -> Float {
+        self.BMI = self.weight! / (self.height! * self.height!);
+        return self.BMI!
+    }
+    
+    func calcStdWight() -> Float {
+        var StdWeight  = (self.height! * self.height!)*22
+        return StdWeight;
+    }
+    
+    //18.5未満で「やせ」18.5以上25未満で「標準」 25以上30未満で「肥満」30以上で「高度肥満」
+    func judge () -> String {
+        if(self.BMI! > 0) {
+            switch (self.calcBMI()){
+                case 0..<18.5:
+                    return "痩せ"
+                case 18.5..<25:
+                    return "標準"
+                case 25..<30:
+                    return "肥満"
+                default:
+                    return "高度肥満"
+            }
+        }else{
+            return ""
+        }
+    }
+}
